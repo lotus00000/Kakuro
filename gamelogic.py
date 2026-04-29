d963b9fecaf3b7d178d351cb6e3e6f5104552711
lotus00000 <alexander@petri.tv>
2026-04-29 18:03:32 +0200

    getrowlength complete

--- gamelogic.py
+++ gamelogic.py
@@ -2,10 +2,9 @@
     # Speicherformat Kakuro
     # Brute Force solver
 class Kakuro:
-    def __init__(self, x, y, difficulty, default_value=0):
+    def __init__(self, x, y, default_value=0):
         self.x = x
         self.y = y
-        self.difficulty = difficulty
         self.contentx = [[default_value for _ in range(x)] for _ in range(y)]
         self.contenty = [[default_value for _ in range(x)] for _ in range(y)]
     def bfill(Kakuro):
@@ -13,18 +12,35 @@ class Kakuro:
             Kakuro.contentx[i][0] = "B"
         for j in range(Kakuro.y):
             Kakuro.contentx[0][j] = "B"   
-    #xplane    
+    #xplane
+        
         for i in range(Kakuro.x):
             Kakuro.contenty[i][0] = "B"
         for j in range(Kakuro.y):
             Kakuro.contenty[0][j] = "B"  
     #yplane
-    def getrowlength(direction, x, y):
-        if(direction=="right"):
-            None
-            #längezähler rechts mit forschleife
-        if(direction=="down"):
-            None
-            #längezähler down mit forschleife
-        
 
+    def getrowlength(self ,direction , x, y):
+        length = 0
+        if(direction == "right"):
+            for i in range(len(self.contentx)-x):
+                if(self.contentx[i+x][y] == 0):
+                    length = length + 1
+                else:
+                    break
+            #längezähler rechts mit forschleife
+
+        if(direction == "down"):
+            for j in range(len(self.contenty[0])-y):
+                if(self.contentx[x][j+y] == 0):
+                    length = length + 1
+                else:
+                    break
+            #längezähler down mit forschleife
+
+        return length
+        
+k = Kakuro(10,10)
+k.bfill()
+print(k.getrowlength("down",5,2))
+print(k.contentx)
